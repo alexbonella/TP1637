03d76194cdf9ca6798cbfd6e07965ef63a94d102
Alexbonella <alexbonella2806@gmail.com>
2019-02-16 15:33:53 -0500

    Prueba TP1637

---
+++ Analisis Peliculas/Prueba_Movies.py
@@ -0,0 +1,16 @@
+#%%
+# Importar las librerias necesarias para resolver el siguiente ejercicio
+
+
+
+#%% Importing the dataset
+# Cargar las siguientes bases de datos, que estan la carpeta "ml-1m"
+# movies ==> movies.dat
+# users ==> users.dat
+# ratings ==> ratings.dat
+#%% Crear un diccionario llamado Usuarios con las siguientes caracteristicas
+# El key es el id del usuario
+# El key lo lleva a una lista que contiene unas tuplas que contienen el nombre de la pelicula vista y el rating obtenido
+
+#%% Sobre el Dataframe de peliculas generar un histograma por generos y cantidad de peliculas del genero 
+# Sobre el dataframe de peliculas generar un histograma por annos, y la cantidad del genero mas visto en ese annos
